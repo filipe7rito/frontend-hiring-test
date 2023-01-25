2b697aec862405c32fb5ab7092b53d7fc972fb41
Filipe Rito <filipe@fidel.uk>
2023-01-25 21:58:00 +0000

    feat(e2e): improve cypress setup

--- phone-test/cypress.config.ts
+++ phone-test/cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    video: false,
+    baseUrl: 'http://localhost:5000',
     viewportWidth: 1280,
     viewportHeight: 720,
     setupNodeEvents(on, config) {}
