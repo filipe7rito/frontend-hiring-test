4e0a0482080b61d3797ad1402fd4bfb3ddb5aa6a
Filipe Rito <filipe@fidel.uk>
2023-01-28 20:30:24 +0000

    feat(calls): fix pagination

--- phone-test/src/pages/CallsList.tsx
+++ phone-test/src/pages/CallsList.tsx
@@ -14,6 +14,7 @@ import {
 import { formatDate, formatDuration } from '../helpers/dates';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { useState } from 'react';
 
 export const PaginationWrapper = styled.div`
   > div {
@@ -24,20 +25,18 @@ export const PaginationWrapper = styled.div`
   }
 `;
 
-const CALLS_PER_PAGE = 5;
-
 export const CallsListPage = () => {
   const [search] = useSearchParams();
   const navigate = useNavigate();
   const pageQueryParams = search.get('page');
+  const [pageSize, setPageSize] = useState(25);
   const activePage = !!pageQueryParams ? parseInt(pageQueryParams) : 1;
 
   const { loading, error, data } = useQuery(PAGINATED_CALLS, {
     variables: {
-      offset: (activePage - 1) * CALLS_PER_PAGE,
-      limit: CALLS_PER_PAGE
+      offset: (activePage - 1) * pageSize,
+      limit: pageSize
     }
-    // onCompleted: () => handleRefreshToken(),
   });
 
   if (loading) return <Spinner />;
@@ -54,71 +53,88 @@ export const CallsListPage = () => {
     navigate(`/calls/?page=${page}`);
   };
 
+  /**
+   * When the page size changes, we need to check if the new page size
+   * will result in a page that is higher than the total number of pages.
+   */
+  const handlePagesizeChange = (newPageSize: number) => {
+    const totalPages = Math.ceil(totalCount / newPageSize);
+    const newActivePage = activePage > totalPages ? totalPages : activePage;
+
+    setPageSize(newPageSize);
+    navigate(`/calls/?page=${newActivePage}`);
+  };
+
   return (
     <>
       <Typography variant="displayM" textAlign="center" py={3}>
         Calls History
       </Typography>
       <Spacer space={3} direction="vertical">
-        {calls.map((call: Call) => {
-          const icon = call.direction === 'inbound' ? DiagonalDownOutlined : DiagonalUpOutlined;
-          const title =
-            call.call_type === 'missed'
-              ? 'Missed call'
-              : call.call_type === 'answered'
-              ? 'Call answered'
-              : 'Voicemail';
-          const subtitle = call.direction === 'inbound' ? `from ${call.from}` : `to ${call.to}`;
-          const duration = formatDuration(call.duration / 1000);
-          const date = formatDate(call.created_at);
-          const notes = call.notes ? `Call has ${call.notes.length} notes` : <></>;
+        <Box overflow={'auto'} maxHeight={'calc(100vh - 220px)'}>
+          {calls.map((call: Call) => {
+            const icon = call.direction === 'inbound' ? DiagonalDownOutlined : DiagonalUpOutlined;
+            const title =
+              call.call_type === 'missed'
+                ? 'Missed call'
+                : call.call_type === 'answered'
+                ? 'Call answered'
+                : 'Voicemail';
+            const subtitle = call.direction === 'inbound' ? `from ${call.from}` : `to ${call.to}`;
+            const duration = formatDuration(call.duration / 1000);
+            const date = formatDate(call.created_at);
+            const notes = call.notes ? `Call has ${call.notes.length} notes` : <></>;
 
-          return (
-            <Box
-              key={call.id}
-              bg="#F7F7F7"
-              boxShadow={5}
-              borderRadius={16}
-              cursor="pointer"
-              onClick={() => handleCallOnClick(call.id)}
-            >
-              <Grid
-                data-testid={`call-item`}
-                gridTemplateColumns="32px 1fr max-content"
-                columnGap={2}
-                borderBottom="1px solid"
-                borderBottomColor="neutral-700"
-                alignItems="center"
-                px={4}
-                py={2}
+            return (
+              <Box
+                key={call.id}
+                bg="#F7F7F7"
+                boxShadow={5}
+                borderRadius={16}
+                padding={2}
+                margin={2}
+                cursor="pointer"
+                onClick={() => handleCallOnClick(call.id)}
               >
-                <Box>
-                  <Icon component={icon} size={32} />
+                <Grid
+                  data-testid={`call-item`}
+                  gridTemplateColumns="32px 1fr max-content"
+                  columnGap={2}
+                  borderBottom="1px solid"
+                  borderBottomColor="neutral-700"
+                  alignItems="center"
+                  px={4}
+                  py={2}
+                >
+                  <Box>
+                    <Icon component={icon} size={32} />
+                  </Box>
+                  <Box>
+                    <Typography variant="body">{title}</Typography>
+                    <Typography variant="body2">{subtitle}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography variant="caption" textAlign="right">
+                      {duration}
+                    </Typography>
+                    <Typography variant="caption">{date}</Typography>
+                  </Box>
+                </Grid>
+                <Box px={4} py={2}>
+                  <Typography variant="caption">{notes}</Typography>
                 </Box>
-                <Box>
-                  <Typography variant="body">{title}</Typography>
-                  <Typography variant="body2">{subtitle}</Typography>
-                </Box>
-                <Box>
-                  <Typography variant="caption" textAlign="right">
-                    {duration}
-                  </Typography>
-                  <Typography variant="caption">{date}</Typography>
-                </Box>
-              </Grid>
-              <Box px={4} py={2}>
-                <Typography variant="caption">{notes}</Typography>
               </Box>
-            </Box>
-          );
-        })}
+            );
+          })}
+        </Box>
       </Spacer>
 
       {totalCount && (
         <PaginationWrapper>
           <Pagination
             activePage={activePage}
-            pageSize={CALLS_PER_PAGE}
+            pageSize={pageSize}
+            onPageSizeChange={handlePagesizeChange}
             onPageChange={handlePageChange}
             recordsTotalCount={totalCount}
           />
